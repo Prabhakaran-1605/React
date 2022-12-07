2b28c72b8f09758c9aa87c092dde741e4641c09d
Prabhakaran@1605 <1605prabhu@gmail.com>
2022-12-07 23:26:45 +0530

    useRef and useReducer Hook

--- hooks-functional-components/src/App.js
+++ hooks-functional-components/src/App.js
@@ -4,30 +4,25 @@ import Message from "./Components/Message"
 import Message1 from "./Components/Message-1"
 import Products from "./Products/Products"
 import DigitalClock from "./Digitalclock/Digitalclock"
-import {BrowserRouter,Route,Switch} from "react-router-dom" 
+import ImageEx from "./Components/ImageEx"
+import MessageEx from "./Components/MessageEx"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 class App extends react.Component {
 
 
   render() {
     return <>
-
       <BrowserRouter>
-      
-      <Navbar/>
-      <Switch>
-<Route path="/product" component={Products}/>
-<Route path="/clock" component={DigitalClock}/>
-
-      </Switch>
-
-
-
+        <Navbar />
+        <Switch>
+        <Route path="/message" component={Message} />
+        <Route path="/message1" component={Message1} />
+          <Route path="/product" component={Products} />
+          <Route path="/clock" component={DigitalClock} />
+          <Route path="/useRef" component={ImageEx} />
+          <Route path="/useReducer" component={MessageEx} />
+        </Switch>
       </BrowserRouter>
-
-      <Message/>
-<hr/>
-      <Message1/>
-
     </>
   }
 }
--- hooks-functional-components/src/Navbar/Navbar.js
+++ hooks-functional-components/src/Navbar/Navbar.js
@@ -7,8 +7,12 @@ let Navbar = ()=>
     <nav className="navbar navbar-dark bg-dark navbar-expand-lg "> <Link to ="/" className="navbar-brand"> Hooks Concept Example </Link>
     <div className = "ml-auto">
     <ul className="navbar-nav">
+    <li className="nav-list"><Link className="nav-link" to="/message">Without-useState</Link></li>
+    <li className="nav-list"><Link className="nav-link" to="/message1">With-useState</Link></li>
     <li className="nav-list"><Link className="nav-link" to="/product">Product</Link></li>
     <li className="nav-list"><Link className="nav-link" to="/clock">DigitalClock</Link></li>
+    <li className="nav-list"><Link className="nav-link" to="/useRef">Hooks-useRef</Link></li>
+    <li className="nav-list"><Link className="nav-link" to="/useReducer">Hooks-useReducer</Link></li>
     </ul>
     </div>
      </nav>
