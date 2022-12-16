d2c0835cd26b255586106e7dfa5f4b7739950b74
Prabhakaran@1605 <1605prabhu@gmail.com>
2022-12-16 23:32:18 +0530

    Redux-thunk-example

--- redux-thunk-example/src/User/User.js
+++ redux-thunk-example/src/User/User.js
@@ -16,13 +16,14 @@ let User = () => {
     let x = useSelector((state) => { return state.users })
     let y = useSelector((state) => { return state.sel_users })
 
-    let useHandler = (singleuser)=>{
+    let useHandler = (singleuser) => {
         console.log(singleuser.id)
-        dispatch(sel_user(singleuser))}
+        dispatch(sel_user(singleuser))
+    }
 
 
     useEffect(() => {
-        dispatch(useraction());  
+        dispatch(useraction());
     }, []);
 
 
@@ -43,21 +44,34 @@ let User = () => {
                         </thead>
                         <tbody>
                             {Object.keys(x).length > 0 ? <>
-                            {x.users.map((singleuser, index) => {
-                                return <tr key={index} onClick={useHandler.bind(this,singleuser)}>
-                                    <td>{singleuser.id}</td>
-                                    <td>{singleuser.firstName}</td>
-                                    <td> <img src={singleuser.image} height="100px" width="100px" alt="#" /></td>
-                                    <td>{singleuser.email}</td>
-                                    <td>{singleuser.gender}</td>
-                                </tr>
-                            })}</> : null}
+                                {x.users.map((singleuser, index) => {
+                                    return <tr key={index} onClick={useHandler.bind(this, singleuser)}>
+                                        <td>{singleuser.id}</td>
+                                        <td>{singleuser.firstName}</td>
+                                        <td> <img src={singleuser.image} height="100px" width="100px" alt="#" /></td>
+                                        <td>{singleuser.email}</td>
+                                        <td>{singleuser.gender}</td>
+                                    </tr>
+                                })}</> : null}
                         </tbody>
                     </table>
                 </div>
                 <div className="col-md-4">
                     <h1>User Details</h1>
                     <pre>{JSON.stringify(y)}</pre>
+                    <div className="card">
+                        <div className="card-header bg-danger">
+                        <img src={y.image} alt="image1" height="100px"/> </div>
+                        <div className="card-body bg-primary" >
+                        <ul className="list-group ">
+                                <li className="list-group-item bg-success text-white"><b>Id: {y.id}</b> </li>
+                                <li className="list-group-item bg-success text-white"><b>FirstName</b> {y.firstName} </li>
+                                <li className="list-group-item bg-success text-white"><b>Last Name:</b> {y.lastName}</li>
+                                <li className="list-group-item bg-success text-white"><b>Age:</b>{y.age}</li>
+                                <li className="list-group-item bg-success text-white"><b>Gender:</b>{y.gender} </li>
+                                </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
